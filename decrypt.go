3cce8b23e9d13daaaa2ce33a5597337863795933
Frank Davidson <davidfr@americas.manulife.net>
2018-01-03 12:46:22 -0500

    Some refinements...

--- decrypt.go
+++ decrypt.go
@@ -21,7 +21,7 @@ type Msg struct {
 }
 
 func main() {
-	const tokenString = `eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJVUkwiOiIiLCJVVUlEIjoiOTBkZmQ2MmMtZjI4My00ZWMwLWI4MjAtZTdiOWM3ZjA3ZTQzIiwicHJvZHVjdF9jb2RlIjoiMDk4NzY1NDMyMTA5ODIiLCJzZXJpYWxfbnVtYmVyIjoiMTIzNDVBWlJRRjEyMzQ1Njc4OTAiLCJiYXRjaF9udW1iZXIiOiJBMUMyRTNHNEk1IiwiZXhwaXJhdGlvbl9kYXRlIjoiMjAxODA3MjEiLCJvdGhlcl9qc29uIjoiJ3t0ZXN0OiIsImp0aSI6IlZlcngiLCJpc3MiOiJ2ZXJ4In0.VwxavGyE1kdZhSdOkfhBCEYMK6f9GJqHyc-irzFlZVCiwJika-fDYn-pky8KF32oWDmLYS8owEebwKipAakf0l0CS6BRmH78TEFTunvjhJt6GIdos54sFldf8psZ_d_C`
+	const tokenString = `eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJVUkwiOiJodHRwczovL3ZlcnguY29kZXMvcHVibGljX2tleS5wZW0iLCJVVUlEIjoiNmI5YzE1OTMtYmRmYy00NWY0LTgzOGYtMjJhODYzNWZlYWY3IiwicHJvZHVjdF9jb2RlIjoiMDk4NzY1NDMyMTA5ODIiLCJzZXJpYWxfbnVtYmVyIjoiMTIzNDVBWlJRRjEyMzQ1Njc4OTAiLCJiYXRjaF9udW1iZXIiOiJBMUMyRTNHNEk1IiwiZXhwaXJhdGlvbl9kYXRlIjoiMjAxODA3MjEiLCJvdGhlcl9pbmZvIjoiJ3t0ZXN0OiIsImp0aSI6IlZlcngiLCJpc3MiOiJ2ZXJ4In0.tQopyHHw0QpSBJOUg2mPlTt3IVV4QIim38G7wA4gFYQmtbF--svGHN8LKh8jM1Oc5yY3G_JmPEoat4BT453urRwcFZoBdkel8HY6YyAhEKusm5AfI9Kahix7C8KUiqx9`
 
 	const pubPEM = `-----BEGIN PUBLIC KEY-----
 MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEW30TKO1egsJFrrn1u0scTn24sDiflGI2
@@ -43,17 +43,31 @@ ikLNBl15qJu2YuGvJbb0hO7UhhhYu5Db
 		return pub, nil
 	})
 
-	if claims, ok := token.Claims.(*Msg); ok && token.Valid {
-		fmt.Printf("%v %v", claims.Expiration, claims.StandardClaims.ExpiresAt)
-		fmt.Println("\nThe token is verified!")
-	} else {
-		fmt.Println(err)
+	fmt.Println("\nIs the JWT token valid? ", token.Valid)
+
+	fmt.Println("\nCustom Claims:")
+
+	claims, ok := token.Claims.(*Msg)
+	if !ok {
+		fmt.Println("Error getting JWT claims!")
 	}
 
-	fmt.Println("token:", token)
+	fmt.Println("\tUrl:", claims.Url)
+	fmt.Println("\tUUID:", claims.UUID)
+	fmt.Println("\tProdCode:", claims.ProdCode)
+	fmt.Println("\tSerNum:", claims.SerNum)
+	fmt.Println("\tBatch:", claims.Batch)
+	fmt.Println("\tExpiration:", claims.Expiration)
+	fmt.Println("\tOtherInfo:", claims.OtherInfo, "\n")
+
+	fmt.Println("Standard Claims:")
+	fmt.Println("\tAudience:", claims.Audience)
+	fmt.Println("\tExpiresAt:", claims.ExpiresAt)
+	fmt.Println("\tId:", claims.Id)
+	fmt.Println("\tIssuedAt:", claims.IssuedAt)
+	fmt.Println("\tIssuer:", claims.Issuer)
+	fmt.Println("\tNotBefore:", claims.NotBefore)
+	fmt.Println("\tSubject:", claims.Subject, "\n")
+
 	fmt.Println("err:", err, "\n")
-
-	fmt.Print("Is the token valid? ")
-
-	fmt.Print(token.Valid)
 }
